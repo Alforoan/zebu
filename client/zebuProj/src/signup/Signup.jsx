86fd04ecaadc69939e63f8e6dad07b5b14497d60
Jay <jdragon2050@gmail.com>
2024-04-05 14:12:28 -0700

    add dynamic label change for when a text is in the input box

--- client/zebuProj/src/signup/Signup.jsx
+++ client/zebuProj/src/signup/Signup.jsx
@@ -8,9 +8,28 @@ const Signup = () => {
     document.getElementById('email').focus();
   };
 
+  const handleChangeEmail = (e) => {
+    const emailLabel = document.getElementById('email-label');
+    if(e.target.value){
+      emailLabel.classList.add('has-value');
+    }else{
+      emailLabel.classList.remove('has-value');
+    }
+  }
+
+  const handleChangePassword = (e) => {
+    const passwordLabel = document.getElementById('password-label');
+    if (e.target.value) {
+      passwordLabel.classList.add('has-value');
+    }else{
+      passwordLabel.classList.remove('has-value');
+    }
+  }
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(e);
   }
 
   return (
@@ -18,12 +37,12 @@ const Signup = () => {
       <Navigation />
       <form className='signup-container' onSubmit={handleSubmit}>
         <div className='input-container'>
-          <input className='form-control'  id='email' type='text'/>
-          <label htmlFor="email">Email</label>
+          <input className='form-control'  id='email' type='text' onChange={handleChangeEmail}/>
+          <label htmlFor="email" id='email-label'>Email</label>
         </div>
         <div className='input-container'>
-          <input className='form-control' id='password' type='text' />
-          <label htmlFor='password'>Password</label>
+          <input className='form-control' id='password' type='text' onChange={handleChangePassword}/>
+          <label htmlFor='password' id='password-label'>Password</label>
         </div>
         <button type='submit'>Signup</button>
       </form>
