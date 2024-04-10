b72ff95885c5b93cab01d685b188dd808c82237b
Jay <jdragon2050@gmail.com>
2024-04-10 09:37:30 -0700

    add refresh token logic and updating users with the refresh token

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,10 @@
 import pool from '../app.js';
 import bcrypt from 'bcrypt';
+import { config } from 'dotenv';
+import jwt from 'jsonwebtoken';
+
+
+config();
 
 async function login(req, res) {
   try {
@@ -11,8 +16,6 @@ async function login(req, res) {
       'SELECT * FROM users WHERE email = $1',
       [email]
     );
-    console.log({ rows });
-    console.log({ rowCount });
     if (rowCount < 1) {
       return res
         .status(400)
@@ -27,10 +30,54 @@ async function login(req, res) {
       if (!passwordMatches) {
         return res.status(400).json({ error: 'Incorrect email or password' });
       }
+      let foundEmail = rows[0]?.email;
+      const accessToken = jwt.sign(
+        { email: foundEmail },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '10s' }
+      );
+      const refreshToken = jwt.sign(
+        { email: foundEmail },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: '1d' }
+      );
+      const otherUsersQuery = {
+        text: 'SELECT * FROM users WHERE email <> $1',
+        values: [foundEmail],
+      };
+      const { rows: otherUsers } = await client.query(otherUsersQuery);
+      const checkColumnQuery = `
+        SELECT EXISTS (
+          SELECT 1 
+          FROM information_schema.columns 
+          WHERE table_name = 'users' 
+          AND column_name = 'refreshtoken'
+        )`;
+
+      const existRows = await client.query(checkColumnQuery);
+
+      if (!existRows?.rows[0]?.exists) {
+        console.log('dont exist rows right');
+        const addColumnQuery = `
+                ALTER TABLE users
+                ADD COLUMN refreshtoken VARCHAR(255);
+                `;
+        await client.query(addColumnQuery);
+      } else {
+        console.log('column already exists');
+      }
+      const updateRefreshTokenQuery = `
+          UPDATE users
+          SET refreshToken = $1
+          WHERE email = $2;
+        `;
+
+      await client.query(updateRefreshTokenQuery, [refreshToken, email]);
       res.status(200).json({ message: 'Sign-in successful' });
     }
   } catch (error) {
     console.log(error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 }
 
