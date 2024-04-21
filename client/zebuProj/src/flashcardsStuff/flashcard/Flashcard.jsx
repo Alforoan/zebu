3b6848d132c669a6355cdaac8f8c348777d36244
Jay <jdragon2050@gmail.com>
2024-04-21 16:56:49 -0700

    add styling and some time logic for implementing spaced repetition logic

--- client/zebuProj/src/flashcardsStuff/flashcard/Flashcard.jsx
+++ client/zebuProj/src/flashcardsStuff/flashcard/Flashcard.jsx
@@ -1,26 +1,78 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react'
+import './Flashcard.css'
 
-const Flashcard = ({card}) => {
+const Flashcard = ({card, onNextCard, isAnswerShown, handleClick}) => {
 
-  const [isAnswerShown, setIsAnswerShown] = useState(false);
+  
+  
 
-  const handleClick = () => {
-    setIsAnswerShown(prev => !prev);
-  }
 
   return (
-    <div>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
       {!isAnswerShown ? (
         <>
-          <p>{card.front}</p>
-          <button onClick={handleClick}>Show Answer</button>
+          <p style={{ marginTop: '2rem' }}>{card.front}</p>
+          <div
+            style={{
+              position: 'fixed',
+              bottom: '6.3rem',
+              left: 0,
+              width: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <button onClick={handleClick}>Show Answer</button>
+          </div>
         </>
       ) : (
         <>
-          <p>{card.front}</p>
+          <p style={{ marginTop: '2rem' }}>{card.front}</p>
           <p>{card.back}</p>
-          <button onClick={handleClick}>Hide Answer</button>
+          <div
+            style={{
+              position: 'fixed',
+              bottom: '2rem',
+              left: 0,
+              width: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <button onClick={handleClick}>Hide Answer</button>
+            <div>
+              <div style={{ display: 'flex', marginTop: '1rem' }}>
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                  <span style={{ textAlign: 'center' }}>&lt;10m</span>
+                  <button onClick={onNextCard} className='difficulty-btn'>
+                    Easy
+                  </button>
+                </div>
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                  <span style={{ textAlign: 'center' }}>&lt;5m</span>
+                  <button onClick={onNextCard} className='difficulty-btn'>
+                    Medium
+                  </button>
+                </div>
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                  <span style={{ textAlign: 'center' }}>&lt;1m</span>
+                  <button onClick={onNextCard} className='difficulty-btn'>
+                    Hard
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </>
       )}
     </div>
--- client/zebuProj/src/flashcardsStuff/flashcards/Flashcards.jsx
+++ client/zebuProj/src/flashcardsStuff/flashcards/Flashcards.jsx
@@ -3,11 +3,14 @@ import axios from 'axios'
 import IsLoggedInContext from '../../context/IsLoggedInProvider';
 import Flashcard from '../flashcard/Flashcard';
 import { useParams } from 'react-router-dom';
+import Navigation from '../../navigation/Navigation';
 
 const Flashcards = () => {
 
   const { setDeckId } = useContext(IsLoggedInContext);
   const [cards, setCards] = useState([]);
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const [isAnswerShown, setIsAnswerShown] = useState(false);
   const {deckId} = useParams();
   console.log({deckId});
 
@@ -39,15 +42,27 @@ const Flashcards = () => {
     fetchData();
   }, [])
 
+  const handleNextCard = async() => {
+    //setIsAnswerShown((prev) => !prev);
+    //setCurrentCardIndex((prevIndex) => prevIndex + 1);
+    const utcTime = new Date().toISOString();
+    console.log({utcTime});
+    const date = new Date(utcTime);
+    date.setUTCSeconds(date.getUTCSeconds() + 10);
+    console.log('new time',date.toISOString());
+
+  };  
+
   const handleClick = () => {
-    
-  }
+    setIsAnswerShown((prev) => !prev);
+  };
 
   return (
     <div>
-      {cards.map((card, index) => (
-        <Flashcard key={index} card={card}/>
-      ))}
+      <Navigation/>
+      {currentCardIndex < cards.length && (
+        <Flashcard card={cards[currentCardIndex]} onNextCard={handleNextCard} handleClick={handleClick} isAnswerShown={isAnswerShown}/>
+      )}
     </div>
   );
 }
